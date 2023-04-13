5fa8fd18ae29397a521166bd1691380baaeb4978
Leonardo Dutra <leodutranew@gmail.com>
2023-04-13 11:21:58 -0300

    #5 - adicionando mais informações as estruturas de dados

--- data/input_data/forecast_input.go
+++ data/input_data/forecast_input.go
@@ -4,7 +4,8 @@ package input_data
 Estrutura de dados de entrada de dados para um conjunto advindo de uma fonte de dados json.
 */
 type ForecastInput struct {
-	Latitude  float64 `json:"latitude"`  // Latitude (localização geográfica)
-	Longitude float64 `json:"longitude"` // Longitude (localização geográfica)
-	Elevation float64 `json:"elevation"` // Nível do mar
+	Latitude       float64        `json:"latitude"`        // Latitude (localização geográfica)
+	Longitude      float64        `json:"longitude"`       // Longitude (localização geográfica)
+	Elevation      float64        `json:"elevation"`       // Nível do mar
+	CurrentWeather CurrentWeather `json:"current_weather"` // Informações do tempo em geral
 }
